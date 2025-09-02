26a1f80e8bbf93aac695b49f1ce0725f3e329654
JayJayArr <JayArr@outlook.de>
2025-09-02 08:09:26 +0200

    add helper to import

--- Reports/NOV_Import_CARDNO_from_CSV.sql
+++ Reports/NOV_Import_CARDNO_from_CSV.sql
@@ -1,7 +1,7 @@
 /**Script to create new cards in a ProWatch System by creating them 
 with bulk badge add and overwriting them 
 with data from a csv inserted into a "translation-table"
-in this case the translation table is called "Novartis" and contains two Columns: CARDNOOLD & CARDNonEW
+in this case the translation table is called "Import" and contains two Columns: CARDNOOLD & CARDNonEW
 */
 USE PWNT;
 
@@ -9,10 +9,10 @@ GO
 UPDATE
     BADGE_C
 SET
-    BADGE_C.CARDNO = Novartis.CARDNONEW
+    BADGE_C.CARDNO = Import.CARDNONEW
 FROM
     BADGE_C
-    INNER JOIN Novartis ON BADGE_C.CARDNO = Novartis.CARDNOOLD;
+    INNER JOIN Import ON BADGE_C.CARDNO = Import.CARDNOOLD;
 
 /** 
 If the Person ID needs to be updated as well the following script can be used
@@ -24,8 +24,23 @@ GO
 UPDATE
     BADGE_V
 SET
-    BADGE_V.BADGE_PERSNO = Novartis.CARDNONEW
+    BADGE_V.BADGE_PERSNO = Import.CARDNONEW
 FROM
     BADGE_C
-    INNER JOIN Novartis ON BADGE_C.CARDNO = Novartis.CARDNonEW
+    INNER JOIN Import ON BADGE_C.CARDNO = Import.CARDNonEW
     INNER JOIN BADGE_V ON BADGE_V.ID = BADGE_C.ID;
+
+
+/**
+Rename the Badge FirstName to the already imported Cardnumber, unrelated to Import
+ */
+USE PWNT;
+
+GO
+UPDATE
+    BADGE
+SET
+    BADGE.FNAME = BADGE_C.CARDNO
+FROM
+    BADGE_C
+    INNER JOIN BADGE ON BADGE.ID = BADGE_C.ID;
