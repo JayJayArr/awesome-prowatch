0bea3c5fd2db9e2519cad334946364eec49cbb43
JayJayArr <JayArr@outlook.de>
2025-11-11 16:03:55 +0100

    fix: baltech mobile ID task sequence

--- Synchronisations/Mobile_ID_export.sql
+++ Synchronisations/Mobile_ID_export.sql
@@ -8,4 +8,11 @@ from BADGE inner join BADGE_C on BADGE.ID = BADGE_C.ID
 where PARTI.DESCRP = 'General Capital Group'
     and EMAIL_PW IS NOT null and EMAIL_PW <> ''
 
+/**
+The Task from Mobile ID is faulty
+Invoke-WebRequest -InFile "./mobileid_gencap.csv" -Uri "https://mobile-id.baltech.de/api/projects/General Capital/sync" -Method Post -Headers @{Authorization="Bearer <Token>"} 
+./ for the file
+Token without Escape Slashes
+File Path
+**/
 
